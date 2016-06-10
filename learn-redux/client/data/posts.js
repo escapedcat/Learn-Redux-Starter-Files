51ca9c3191279a45b167ceb72e586ae2af9bbc2b
hannes <you@example.com>
2016-06-10 15:54:38 +0800

    try to make sagas work - WIP

--- learn-redux/client/data/posts.js
+++ learn-redux/client/data/posts.js
@@ -1,12 +1,14 @@
 const API_ENDPOINT = `http://localhost:3000/posts`;
 
 export const fetchPosts = () => {
-  return fetch(API_ENDPOINT).then(function (response) {
-    return response.json().then(function (json) {
-console.dir(json);
-      // return json.photos.photo.map(
-      //   ({farm, server, id, secret}) => `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`
-      // );
+console.info('fetchPosts');
+    return fetch(API_ENDPOINT).then(function (response) {
+        return response.json().then(function (json) {
+    console.dir(json);
+            return json;
+          // return json.photos.photo.map(
+          //   ({farm, server, id, secret}) => `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`
+          // );
+        })
     })
-  })
 };
--- learn-redux/client/sagas/sagas.js
+++ learn-redux/client/sagas/sagas.js
@@ -1,7 +1,10 @@
 import { fetchPosts } from '../data/posts.js';
 import { put, take } from 'redux-saga/effects';
 
+// fetchPosts();
+
 export function* loadPosts() {
+console.info('loadPosts');
   try {
     const Posts = yield fetchPosts();
     yield put({type: 'POSTS_LOADED', Posts});
